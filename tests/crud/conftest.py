949bec4467cefe8347c60b872a819fe8e422f827
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-06-26 11:28:23 +0200

    Make session fixture begin and rollback transactions

--- tests/crud/conftest.py
+++ tests/crud/conftest.py
@@ -1,6 +1,7 @@
 from typing import Generator
 
 import pytest
+import sqlalchemy as sa
 from sqlmodel import Session
 
 from src.api.database import db
@@ -11,7 +12,23 @@ DATABASE_URL = f"sqlite:///app/tests/{SQLITE_FILE_NAME}"
 
 @pytest.fixture(scope="session")
 def session() -> Generator[Session, None, None]:
+    """Provide a session that will automatically create and rollback transactions."""  # https://github.com/tiangolo/sqlmodel/discussions/940
     db.set_up_db_engine()
-    with Session(db.ENGINE) as session:
-        yield session
-        session.rollback()
+
+    connection = db.ENGINE.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
+
+    nested = connection.begin_nested()
+
+    @sa.event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()

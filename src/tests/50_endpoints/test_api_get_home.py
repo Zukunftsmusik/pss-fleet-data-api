b20c4d4e9616c60c0432bdf4440571599b744087
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-11 14:13:16 +0200

    Refactor home endpoint

--- src/tests/50_endpoints/test_api_get_home.py
+++ src/tests/50_endpoints/test_api_get_home.py
@@ -6,4 +6,4 @@ def test_get_home(client: TestClient):
         response = client.get("/")
         assert response.status_code == 200
         with open("src/api/html/home.html", "r") as fp:
-            assert response.read().decode("utf-8") == fp.read()
+            assert response.text == fp.read()

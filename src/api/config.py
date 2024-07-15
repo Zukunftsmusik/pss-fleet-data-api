86ca3765546c726ebe7d5561cca81fcff63b7253
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2024-07-15 13:21:05 +0200

    Update version

--- src/api/config.py
+++ src/api/config.py
@@ -21,7 +21,7 @@ class Settings:
 
     # Base
     project_name: str = "PSS Fleet Data API"
-    version: str = "1.2.0"
+    version: str = "1.3.0"
     description: str = "An API server for Pixel Starships Fleet Data."
     contact = {
         "email": "theworstpss@gmail.com",

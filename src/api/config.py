94cf0047ba8d32f68e17cb290310ea4b770e868b
The worst. <5472896+Zukunftsmusik@users.noreply.github.com>
2024-08-22 12:31:43 +0200

    Hotfix/1.4.1 (#15)
    * Allow empty user names in out models and schema V9 (for users who deleted their account)

--- src/api/config.py
+++ src/api/config.py
@@ -22,7 +22,7 @@ class Settings:
 
     # Base
     project_name: str = "PSS Fleet Data API"
-    version: str = "1.4.0"
+    version: str = "1.4.1"
     description: str = "An API server for Pixel Starships Fleet Data."
     contact = {
         "email": "theworstpss@gmail.com",
